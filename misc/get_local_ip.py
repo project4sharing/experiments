4c4272079cdb903d58372b4d07fc032c4f8d8c15
pl <petelam@yahoo.com>
2026-05-11 11:57:11 -0700

    add misc

---
+++ misc/get_local_ip.py
@@ -0,0 +1,15 @@
+import socket
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Doesn't even have to be reachable
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+print(f"Local IP: {get_local_ip()}")
